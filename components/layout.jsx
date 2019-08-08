1f9d33e37dbad1a208b9a0da0c2442ee8251a5db
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-08-08 22:46:52 +0700

    :sparkles: Add footer + Edit styles

--- components/layout.jsx
+++ components/layout.jsx
@@ -6,10 +6,16 @@ export default ({ children }) => (
       {`
         * {
           box-sizing: border-box;
+          font-family: monospace;
         }
         body {
           color: #4a4a4a;
           background-color: #f8f8f8;
+          padding: 2rem;
+          text-align: center;
+        }
+        a {
+          color: inherit!important;
         }
         input {
           width: 100%;
@@ -20,14 +26,37 @@ export default ({ children }) => (
         }
         button {
           color: #ecf0f1;
-          margin-top: 1rem;
-          background: #009688;
+          margin: 1rem;
+          background: #4a4a4a;
           border: none;
           padding: 1rem;
+        }
+        footer {
+          padding: 3rem 1.5rem 6rem;
         }
       `}
 
     </style>
     { children }
+    <footer>
+      <p className="is-italic">
+        Made with
+        {' '}
+        <span role="img" aria-label="Love">❤️</span>
+        ,
+        {' '}
+        <span role="img" aria-label="Fire">🔥</span>
+        , and a keyboard by
+        {' '}
+        <a href="https://www.hoangvvo.com/">Hoang Vo</a>
+        .
+      </p>
+      <p>
+        Source code is on
+        {' '}
+        <a href="https://github.com/hoangvvo/nextjs-mongodb-app">Github</a>
+        .
+      </p>
+    </footer>
   </>
 );
