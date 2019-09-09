038fd2f24c32b4848ab61e89b783a3c2371e162e
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-09-08 22:49:05 -0400

    Remove Back to homepage

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from 'react';
 import axioswal from 'axioswal';
-import Link from 'next/link';
 import { UserContext } from '../components/UserContext';
 import Layout from '../components/layout';
 import redirectTo from '../lib/redirectTo';
@@ -30,7 +29,6 @@ const SignupPage = () => {
   return (
     <Layout>
       <div>
-        <Link href="/"><a>Back to homepage</a></Link>
         <h2>Sign up</h2>
         <form onSubmit={handleSubmit}>
           <div>
