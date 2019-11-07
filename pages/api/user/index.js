01da8b55fed9a833c18ca4909af9f316e9af9c75
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-11-06 19:13:49 -0500

    Migrate to nextConnect

--- pages/api/user/index.js
+++ pages/api/user/index.js
@@ -1,22 +1,22 @@
+import nextConnect from 'next-connect';
 import withMiddleware from '../../../middlewares/withMiddleware';
 
-const handler = (req, res) => {
-  if (req.method === 'PATCH') {
-    if (!req.user) return res.status(401).send('You need to be logged in.');
-    const { name, bio } = req.body;
-    return req.db
-      .collection('users')
-      .updateOne({ _id: req.user._id }, { $set: { name, bio } })
-      .then(() => res.json({
-        message: 'Profile updated successfully',
-        data: { name, bio },
-      }))
-      .catch(error => res.send({
-        status: 'error',
-        message: error.toString(),
-      }));
-  }
-  return res.status(405).end();
-};
+const handler = nextConnect();
+
+handler.patch((req, res) => {
+  if (!req.user) return res.status(401).send('You need to be logged in.');
+  const { name, bio } = req.body;
+  return req.db
+    .collection('users')
+    .updateOne({ _id: req.user._id }, { $set: { name, bio } })
+    .then(() => res.json({
+      message: 'Profile updated successfully',
+      data: { name, bio },
+    }))
+    .catch(error => res.send({
+      status: 'error',
+      message: error.toString(),
+    }));
+});
 
 export default withMiddleware(handler);
--- pages/api/users.js
+++ pages/api/users.js
@@ -1,43 +1,43 @@
+import nextConnect from 'next-connect';
 import isEmail from 'validator/lib/isEmail';
 import bcrypt from 'bcryptjs';
 import withMiddleware from '../../middlewares/withMiddleware';
 
-const handler = (req, res) => {
-  if (req.method === 'POST') {
-    const { email, name, password } = req.body;
-    if (!isEmail(email)) {
-      return res.send({
-        status: 'error',
-        message: 'The email you entered is invalid.',
-      });
-    }
-    return req.db
-      .collection('users')
-      .countDocuments({ email })
-      .then((count) => {
-        if (count) {
-          return Promise.reject(Error('The email has already been used.'));
-        }
-        return bcrypt.hashSync(password);
-      })
-      .then(hashedPassword => req.db.collection('users').insertOne({
-        email,
-        password: hashedPassword,
-        name,
-      }))
-      .then((user) => {
-        req.session.userId = user.insertedId;
-        res.status(201).send({
-          status: 'ok',
-          message: 'User signed up successfully',
-        });
-      })
-      .catch(error => res.send({
-        status: 'error',
-        message: error.toString(),
-      }));
+const handler = nextConnect();
+
+handler.post((req, res) => {
+  const { email, name, password } = req.body;
+  if (!isEmail(email)) {
+    return res.send({
+      status: 'error',
+      message: 'The email you entered is invalid.',
+    });
   }
-  return res.status(405).end();
-};
+  return req.db
+    .collection('users')
+    .countDocuments({ email })
+    .then((count) => {
+      if (count) {
+        return Promise.reject(Error('The email has already been used.'));
+      }
+      return bcrypt.hashSync(password);
+    })
+    .then(hashedPassword => req.db.collection('users').insertOne({
+      email,
+      password: hashedPassword,
+      name,
+    }))
+    .then((user) => {
+      req.session.userId = user.insertedId;
+      res.status(201).send({
+        status: 'ok',
+        message: 'User signed up successfully',
+      });
+    })
+    .catch(error => res.send({
+      status: 'error',
+      message: error.toString(),
+    }));
+});
 
 export default withMiddleware(handler);
