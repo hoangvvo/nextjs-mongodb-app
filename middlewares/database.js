feac08f89056890154a45d34f4ea312e667b90c6
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-11-06 20:00:09 -0500

    Add useUnifiedTopology to remove depr message

--- middlewares/database.js
+++ middlewares/database.js
@@ -1,6 +1,9 @@
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true });
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 export default function database(req, res, next) {
   if (!client.isConnected()) {
