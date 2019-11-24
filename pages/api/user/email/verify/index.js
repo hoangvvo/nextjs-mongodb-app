4d32e8fd27f6dd89663564de69d8dc04a558df08
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-11-23 23:22:05 -0500

    Add Email verification

---
+++ pages/api/user/email/verify/index.js
@@ -0,0 +1,36 @@
+import crypto from 'crypto';
+import sgMail from '@sendgrid/mail';
+import nextConnect from 'next-connect';
+import middleware from '../../../../../middlewares/middleware';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.post(async (req, res) => {
+  if (!req.user) return res.status(401).send('You need to be logged in.');
+  const token = crypto.randomBytes(32).toString('hex');
+  await req.db
+    .collection('tokens')
+    .insertOne({
+      token,
+      userId: req.user._id,
+      type: 'emailVerify',
+    });
+  const msg = {
+    to: req.user.email,
+    from: process.env.SENDGRID_EMAIL_FROM,
+    templateId: process.env.SENDGRID_TEMPLATEID_EMAILVERIFY,
+    dynamic_template_data: {
+      subject: '[nextjs-mongodb-app] Please verify your email address.',
+      name: req.user.name,
+      url: `${process.env.WEB_URI}/api/user/email/verify/${token}`,
+    },
+  };
+  await sgMail.send(msg);
+  return res.json({ message: 'An email has been sent to your inbox.' });
+});
+
+export default handler;
