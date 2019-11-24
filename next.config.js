31c51b20e6905a1ed63f0701ec376ec1baf3d71f
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-11-23 23:20:22 -0500

    Add and include new env var

--- next.config.js
+++ next.config.js
@@ -5,5 +5,9 @@ module.exports = {
     MONGODB_URI: process.env.MONGODB_URI,
     CLOUDINARY_URL: process.env.CLOUDINARY_URL,
     DB_NAME: process.env.DB_NAME,
+    WEB_URI: process.env.WEB_URI,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    SENDGRID_TEMPLATEID_EMAILVERIFY: process.env.SENDGRID_TEMPLATEID_EMAILVERIFY,
+    SENDGRID_EMAIL_FROM: process.env.SENDGRID_EMAIL_FROM,
   },
 };
