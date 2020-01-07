c9cf0b23aaf9f81aec72c31e652d12c4290703dc
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-07 17:44:38 -0500

    Disable stringify connect-mongo

--- middlewares/session.js
+++ middlewares/session.js
@@ -4,5 +4,9 @@ import connectMongo from 'connect-mongo';
 const MongoStore = connectMongo(session);
 
 export default function (req, res, next) {
-  return session({ store: new MongoStore({ client: req.dbClient }) })(req, res, next);
+  return session({
+    store: new MongoStore({
+      client: req.dbClient, stringify: false,
+    }),
+  })(req, res, next);
 }
