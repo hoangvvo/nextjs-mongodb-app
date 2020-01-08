95f957a4715b41e2049f42fbaa79634f0f6eb5d3
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-07 23:28:09 -0500

    prettier

--- pages/api/users.js
+++ pages/api/users.js
@@ -12,11 +12,12 @@ handler.post(async (req, res) => {
   try {
     if (!isEmail(email)) throw new Error('The email you entered is invalid.');
     if (!password || !name) throw new Error('Missing field(s)');
-    if ((await req.db
-      .collection('users')
-      .countDocuments({ email })) > 0) throw new Error('The email has already been used.');
+    if ((await req.db.collection('users').countDocuments({ email })) > 0) throw new Error('The email has already been used.');
     const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await req.db.collection('users').insertOne({ email, password: hashedPassword, name }).then(({ ops }) => ops[0]);
+    const user = await req.db
+      .collection('users')
+      .insertOne({ email, password: hashedPassword, name })
+      .then(({ ops }) => ops[0]);
     req.logIn(user, (err) => {
       if (err) throw err;
       res.status(201).json({
