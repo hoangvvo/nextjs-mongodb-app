3eebc80fdc912883d8b1fb1e9c262640ca05c6a4
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-07 21:52:10 -0500

    Use passportjs

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,12 +1,13 @@
 import nextConnect from 'next-connect';
 import database from './database';
 import session from './session';
-import authentication from './authentication';
+import passport from '../lib/passport';
 
 const middleware = nextConnect();
 
 middleware.use(database);
 middleware.use(session);
-middleware.use(authentication);
+middleware.use(passport.initialize());
+middleware.use(passport.session());
 
 export default middleware;
