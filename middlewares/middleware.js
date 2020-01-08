50d42273ecaaafd8622eb7d430bbf2e7f0220b09
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-07 22:33:01 -0500

    Add compat for req.redirect

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -2,9 +2,11 @@ import nextConnect from 'next-connect';
 import database from './database';
 import session from './session';
 import passport from '../lib/passport';
+import compat from './compat';
 
 const middleware = nextConnect();
 
+middleware.use(compat);
 middleware.use(database);
 middleware.use(session);
 middleware.use(passport.initialize());
