81513af9519f9424793d34c262fc9a5818356d34
Hoang Vo <dev+g@hvvo.dev>
2021-09-21 00:36:01 +0700

    Bump next-session and set cookie expiry

--- api-lib/middlewares/session.js
+++ api-lib/middlewares/session.js
@@ -6,5 +6,15 @@ export default function session(req, res, next) {
     client: req.dbClient,
     stringify: false,
   });
-  return nextSession({ store: mongoStore })(req, res, next);
+  return nextSession({
+    store: mongoStore,
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 2 * 7 * 24 * 60 * 60, // 2 weeks,
+      path: '/',
+      sameSite: 'strict',
+    },
+    touchAfter: 1 * 7 * 24 * 60 * 60 * 1000, // 1 week
+  })(req, res, next);
 }
